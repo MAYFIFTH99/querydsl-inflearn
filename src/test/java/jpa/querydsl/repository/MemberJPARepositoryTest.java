f0b3dda172b7fb8f231a032a809216ab1013b424
harvey <alstjr971@naver.com>
2024-12-18 16:16:21 +0900

    test : memberJPARepository test

--- src/test/java/jpa/querydsl/repository/MemberJPARepositoryTest.java
+++ src/test/java/jpa/querydsl/repository/MemberJPARepositoryTest.java
@@ -1,4 +1,73 @@
+package jpa.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import jpa.querydsl.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+
 class MemberJPARepositoryTest {
-  
+
+//    @Autowired
+//    EntityManager em;
+
+    @Autowired
+    MemberJPARepository memberJPARepository;
+
+    @Test
+    @DisplayName("save")
+    void save() throws Exception {
+        //given
+        Member member = new Member();
+        //when
+        memberJPARepository.save(member);
+        //then
+        assertThat(member.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("findById")
+    void findById() throws Exception {
+        //given
+        Member member = new Member();
+        memberJPARepository.save(member);
+        //when
+        Member findMember = memberJPARepository.findById(member.getId()).get();
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+    }
+
+    @Test
+    void findAll() throws Exception {
+        //given
+        List<Member> members = new ArrayList<>();
+        Member member1 = new Member();
+        Member member2 = new Member();
+        Member member3 = new Member();
+
+        members.add(member1);
+        members.add(member2);
+        members.add(member3);
+        memberJPARepository.save(member1);
+        memberJPARepository.save(member2);
+        memberJPARepository.save(member3);
+        //when
+        List<Member> memberList = memberJPARepository.findAll();
+        //then
+        assertThat(memberList).hasSize(3);
+    }
+
 }
