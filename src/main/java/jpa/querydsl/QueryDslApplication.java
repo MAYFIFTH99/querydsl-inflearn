4a4beda144366f786015b2537dd912eb4aa81d30
harvey <alstjr971@naver.com>
2024-12-18 16:32:38 +0900

    no message

--- src/main/java/jpa/querydsl/QueryDslApplication.java
+++ src/main/java/jpa/querydsl/QueryDslApplication.java
@@ -1,8 +1,11 @@
 package jpa.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import jpa.querydsl.entity.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QueryDslApplication {
@@ -11,5 +14,9 @@ public class QueryDslApplication {
         SpringApplication.run(QueryDslApplication.class, args);
     }
 
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 
 }
